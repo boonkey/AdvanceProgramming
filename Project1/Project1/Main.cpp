edb315d6a7de1b2095c76223bf977d339a5a9d96
tomer <tomer5404@gmail.com>
2017-04-08 16:07:00 +0300

    add comments and guidling on rest of work

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -4,25 +4,37 @@
 int main()
 {
 	Board mainGameBoard(10, 10);
-	//TODO add file read and format checks
+	//TODO add file read and format checks (should be in fileParsing.cpp)
 	Player A(10, 10);
-	//TODO fix unable to open file
+	Player B(10, 10);
+
+	//TODO fix unable to open file - change file name and location as needed
 	pair<vector<pair<int, int>>, int> A_attacks = parseAttackFile(".\clean_movesA.attack-a");
 	if( A_attacks.second == 0 )	{ //functions ended as expected
 		A.setlistOfAttacks(A_attacks.first);
 	}
+	pair<vector<pair<int, int>>, int> B_attacks = parseAttackFile(".\clean_movesB.attack-b");
+	if (A_attacks.second == 0) { //functions ended as expected
+		A.setlistOfAttacks(A_attacks.first);
+	}
 
+
+/*	examples to see the board is working
 	mainGameBoard.set(1, 2, 'c');
 	mainGameBoard.set(1, 1, 'x');
 	mainGameBoard.set(2, 2, 'c');
 	mainGameBoard.set(7, 2, 'c');
 	mainGameBoard.set(9, 6, 'p');
 	mainGameBoard.set(9, 9, 'x');
-	//mainGameBoard.print();
+	mainGameBoard.print();
+	*/
 
+	//TODO change to send only A part of the board (same for B)
 	A.setBoard(mainGameBoard.getFullBoard(), 10, 10);
-	//A.printBoard();
+	//B.setBoard(mainGameBoard.getFullBoard(), 10, 10);
 
+	//fill rest of game logic
+	//while true: ask for player attack , notify players on attack result , keep score
 
 
 	//cout << "hello" << endl;
@@ -33,27 +45,4 @@ int main()
 
 	return 0;
 }
-
-/*
-pair<vector<pair<int,int>>,int> parseAttackFile(string filename){
-	vector<pair<int, int>> results;
-	string line;
-	smatch m;
-	ifstream myfile(filename);
-	regex exp("\s*(\d*)\s*,\s*(\d*)");
-	if (myfile.is_open()) {
-		while (getline(myfile, line)) {
-			if(regex_search(line, m, exp)){
-				for (auto x : m) cout << x << " ";
-				cout << endl;
-			}
-		}
-		myfile.close();
-	}
-	else cout << "Unable to open file";
-	return make_pair(results, 0);
-}
-
-
-*/
 
