ec0680238c3610f5f2e8f9192cdafbcfdb762904
boonkey <dansheinin@gmail.com>
2017-04-12 18:08:02 +0300

    Added support of reading board
    data_types.h  added full support of loading a gameboard from file to
    Board
    main.h          added constant BOARD_SIZE = 10

--- Project1/Project1/data_types.h
+++ Project1/Project1/data_types.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <iostream>
 #include <vector>
-
 #include <fstream>
+#include <string>
+#include <cerrno>
+#include <Windows.h>
+#define BOARD_SIZE 10
+
 using namespace std;
 
 class Board{
@@ -34,10 +38,10 @@ public:
 		delete[] board;	
 	}
 
-	char get(int x, int y) { return board[y - 1][x - 1]; }
+	char get(int col, int row) { return board[col - 1][row - 1]; }
 	const char** getFullBoard(){ return const_cast<const char **>(board) ; }
-	void set(int x, int y, char c) { board[y-1][x-1] = c; }
-	void print(){
+	void set(int col, int row, char c) { board[col - 1][row - 1] = c; }
+	void Board::print(){
 		for (int i = 0; i < numOfCols; i++) {
 			for (int j = 0; j < numOfRows; j++) {
 				cout << board[i][j] << ' ';
@@ -45,10 +49,30 @@ public:
 			cout << endl;
 		}
 	}
-	
 
-
-
+	int Board::loadFromFile(string filename) { //loads board from file. returns 0 on success. -1 on fail
+		string line;
+		int rows = 1;
+		char buffer[MAX_PATH];
+		GetModuleFileName(NULL, buffer, MAX_PATH);
+		string::size_type pos = string(buffer).find_last_of("\\/");
+		cout << "Working Dir: " << string(buffer).substr(0, pos) << endl; 
+		ifstream boardFile(filename);
+		if (boardFile.is_open()) {
+			while (getline(boardFile, line) && rows < BOARD_SIZE) {
+				for (int i = 1; i <= BOARD_SIZE; i++) {
+					set(i, rows, line.length() >= i ? line[i] : ' ');
+				}
+				rows++;
+			}
+			print();
+			return 0;
+		}
+		else {
+			printf("Unable to open (file is close) %s\n ", filename.c_str());
+			return -1;
+		}
+	}
 };
 
 
